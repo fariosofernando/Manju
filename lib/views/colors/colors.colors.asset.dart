d0d359cecc1135f82b160f1ea7c1d2052ab49453
farioso-ferando <farioso.f@gmail.com>
2021-11-26 16:42:54 +0200

    adicionando ultimas modificacoes

--- lib/views/colors/colors.colors.asset.dart
+++ lib/views/colors/colors.colors.asset.dart
@@ -2,5 +2,3 @@ import 'package:flutter/material.dart';
 
 const Color manjuPrimaryColor = Color.fromRGBO(255, 4, 41, 1);
 const Color manjuPrimaryWhiteColor = Color.fromRGBO(255, 4, 41, .035);
-const Color manjuSecundaryColor = Colors.brown;
-const Color manjuDisableColor = Colors.grey;
