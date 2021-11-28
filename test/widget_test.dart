c622836a5dd1110a1eddc2c5b1bb7bd8bec8670f
farioso-ferando <farioso.f@gmail.com>
2021-11-28 18:41:05 +0200

    Testado modelo de mesagens

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,38 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+import 'package:manju/controllers/sms.controller.dart';
+import 'package:manju/models/mesage.model.dart';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+void main() {
+  // Mesage sms = Sms.listSms[0];
+  // print('Enviado por: ${sms.toName}');
+  // print('Data e hora: ${sms.date} ${sms.time}');
+  // print('Para: ${sms.forIn}');
+  // print('Titulo: ${sms.titleMesage}');
+  // print('Mesagem: ${sms.bodyMesage}');
+  //
+  // ignore: avoid_print
+  print('QutSms: ${smsNaoLidas()}');
+}
 
-// import 'package:manju/main.dart';
+int smsNaoLidas() {
+  List<Mesage> notReadSms = []; // lista de mesagens nao lidas.
+  List<Mesage> yesReadSms = []; // lista de mesagens lidas.
+  var x = 0;
+  for (var i in Sms.listSms) {
+    i.readMeasage == false ? notReadSms.add(i) : yesReadSms.add(i);
+    x += 1;
+  }
 
-// void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
-
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
-// }
+  int numYes = 0;
+  int numNot = 0;
+  for (var yes in yesReadSms) {
+    numYes += 1;
+  }
+  print('reading: ${numYes}');
+  // ignore: unused_local_variable
+  for (var not in notReadSms) {
+    numNot += 1;
+  }
+  // ignore: avoid_print
+  print('not reading: ${numNot}');
+  return x;
+}
